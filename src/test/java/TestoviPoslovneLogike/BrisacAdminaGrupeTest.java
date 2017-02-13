909375594a1278085aa03c85fa7b586646310047
Filip Ivanko <filipivanko@gmail.com>
2017-02-13 17:31:17 +0100

    dodan Unittest za brisacAdminaGrupe.java BrisacAdminaGrupeTest.java

---
+++ src/test/java/TestoviPoslovneLogike/BrisacAdminaGrupeTest.java
@@ -0,0 +1,73 @@
+package TestoviPoslovneLogike;
+
+import DAO.Repozitorij;
+import Model.Admin;
+import PoslovnaLogika.BrisacAdminaGrupe;
+import PoslovnaLogika.RootAdmnStranicaManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class BrisacAdminaGrupeTest {
+    
+    @Test
+    public void testBrisanjeAdminaGrupeAkoJeOdabraniAdminGrupeUSessionuNull() {
+        
+        HttpSession session = mock(HttpSession.class);
+        Repozitorij repo = mock(Repozitorij.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        Admin odabraniAdminKompanijeUSessionu = null;
+        
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("odabraniAdminGrupa")).thenReturn(odabraniAdminKompanijeUSessionu);
+        
+        BrisacAdminaGrupe brisac = new BrisacAdminaGrupe();
+        brisac.obrisiAdminaGrupe(request, repo);
+        
+        RootAdmnStranicaManager rootAdminStranica = new RootAdmnStranicaManager();
+        rootAdminStranica.azurirajRootAdminStranicu(request, repo);
+        verify(repo,never()).obrisi(any(Admin.class));
+        
+    }
+        @Test
+    public void testBrisanjeAdminaGrupeAkoPostojiOdabraniAdminGrupeUSessionu() {
+        
+        HttpSession session = mock(HttpSession.class);
+        Repozitorij repo = mock(Repozitorij.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        Admin odabraniAdminKompanijeUSessionu = mock(Admin.class);
+        
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("odabraniAdminGrupa")).thenReturn(odabraniAdminKompanijeUSessionu);
+        
+        BrisacAdminaGrupe brisac = new BrisacAdminaGrupe();
+        brisac.obrisiAdminaGrupe(request, repo);
+        
+        RootAdmnStranicaManager rootAdminStranica = new RootAdmnStranicaManager();
+        rootAdminStranica.azurirajRootAdminStranicu(request, repo);
+        verify(repo,times(1)).obrisi(odabraniAdminKompanijeUSessionu);
+        
+    }
+            @Test
+    public void testBrisanjeAdminaUpdateSessionaGrupeAkoPostojiOdabraniAdminGrupeUSessionu() {
+        
+        HttpSession session = mock(HttpSession.class);
+        Repozitorij repo = mock(Repozitorij.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        Admin odabraniAdminKompanijeUSessionu = mock(Admin.class);
+        
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("odabraniAdminGrupa")).thenReturn(odabraniAdminKompanijeUSessionu);
+        
+        BrisacAdminaGrupe brisac = new BrisacAdminaGrupe();
+        brisac.obrisiAdminaGrupe(request, repo);
+        
+        RootAdmnStranicaManager rootAdminStranica = new RootAdmnStranicaManager();
+        rootAdminStranica.azurirajRootAdminStranicu(request, repo);
+       
+        verify(session,times(1)).setAttribute("odabraniAdminGrupa",null);
+        
+    }
+}
