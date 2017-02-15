44e473027fecdf0bfdeda4a1d4e653e8e675f7b2
Filip Ivanko <filipivanko@gmail.com>
2017-02-15 13:12:55 +0100

    dodan dependecy za AspectJ u Pom.xml

--- src/main/java/Model/Grupa.java
+++ src/main/java/Model/Grupa.java
@@ -1,22 +1,17 @@
 package Model;
 
-import com.sun.javafx.scene.control.skin.VirtualFlow;
+
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
+
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
